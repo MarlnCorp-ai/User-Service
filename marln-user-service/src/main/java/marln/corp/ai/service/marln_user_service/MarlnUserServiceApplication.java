16eb2fc8507f87ffd3481dec8eba15ffbbaa17f3
Debasish <debasida08@gmail.com>
2025-04-12 00:23:26 +0530

    project structure created with basic properties

---
+++ marln-user-service/src/main/java/marln/corp/ai/service/marln_user_service/MarlnUserServiceApplication.java
@@ -0,0 +1,13 @@
+package marln.corp.ai.service.marln_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarlnUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarlnUserServiceApplication.class, args);
+	}
+
+}
